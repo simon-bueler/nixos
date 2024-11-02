d1ac8b1d787ad03a6f3e1e472cd0d477643dd8a3
simon-bueler <simon.bueeler@icloud.com>
2024-11-02 17:24:34 +0100

    add another host

---
+++ hosts/nixSer4/home.nix
@@ -0,0 +1,86 @@
+{ pkgs, config, ... }: {
+
+  imports = [
+    ./variables.nix
+
+    # Programs
+    ../../home/programs/kitty
+    ../../home/programs/qutebrowser
+    ../../home/programs/shell
+    ../../home/programs/git
+    ../../home/programs/yazi
+    ../../home/programs/markdown
+    ../../home/programs/thunar
+    ../../home/programs/zed-editor
+
+    # Scripts
+    ../../home/scripts # All scripts
+
+    # System (Desktop environment like stuff)
+    ../../home/system/hyprland
+    ../../home/system/hypridle
+    ../../home/system/hyprlock
+    ../../home/system/hyprpanel
+    ../../home/system/hyprpaper
+    # ../../home/system/hyprswitch
+    ../../home/system/gtk
+    ../../home/system/wofi
+    ../../home/system/batsignal
+    ../../home/system/zathura
+    ../../home/system/mime
+    ../../home/system/udiskie
+    ../../home/system/clipman
+
+  ];
+
+  home = {
+    inherit (config.var) username;
+    homeDirectory = "/home/" + config.var.username;
+
+    packages = with pkgs; [
+      # Apps
+      brave
+      discord
+      vlc
+      feishin
+      element-desktop
+
+      # Dev
+      # go
+      # nodejs
+      # python3
+      # jq
+      # figlet
+
+      # Utils
+      zip
+      unzip
+      optipng
+      pfetch
+      pandoc
+      btop
+      lunarvim
+
+      # Just cool
+      peaclock
+      cbonsai
+      pipes
+      cmatrix
+      cava
+
+      # Backup
+      firefox
+    ];
+
+    # Import my profile picture, used by the hyprpanel dashboard
+    file.".profile_picture.png" = { source = ./profile_picture.jpeg; };
+
+    # Import my wallpaper, used by the hyprpanel dashboard
+    #file.".wallpaper.png" = { source = ./wallpaper.png; };
+
+    # Don't touch this
+    stateVersion = "24.05";
+  };
+
+  programs.home-manager.enable = true;
+}
---
+++ hosts/nixSer4/variables.nix
@@ -0,0 +1,45 @@
+{ config, ... }: {
+  imports = [ ../../nixos/variables-config.nix ];
+
+  config.var = {
+    hostname = "nixvm";
+    username = "simon";
+    password = "$y$j9T$KWxCw6L0CU.CezL75MlL7.$eNq00ZxucT8ioeXcf995nULGlTzvFGXtv9IknpU6602";
+    sshAuthKey = "";
+    configDirectory = "/home/" + config.var.username
+      + "/.config/nixos"; # The path of the nixos configuration directory
+
+
+    keyboardLayout = "ch";
+    consoleLayout = "sg";
+
+    location = "Luzern";
+    timeZone = "Europe/Zurich";
+    defaultLocale = "de_CH.UTF-8";
+    extraLocale = "en_US.UTF-8";
+
+    git = {
+      username = "simon-bueler";
+      email = "simon.bueeler@icloud.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = false;
+
+    theme = {
+      rounding = 15;
+      gaps-in = 3;
+      gaps-out = 3 * 2;
+      active-opacity = 1;
+      inactive-opacity = 0.8;
+      blur = true;
+      border-size = 1;
+      animation-speed = "medium"; # "fast" | "medium" | "slow"
+
+      bar = {
+        transparent = true;
+        floating = true;
+      };
+    };
+  };
+}
