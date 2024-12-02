e96e54b9a09b23705114cc764055ca2f7bc8312f
simon-bueler <simon.bueeler@icloud.com>
2024-12-02 13:45:41 +0100

    update flake

--- themes/nixy.nix
+++ themes/nixy.nix
@@ -37,7 +37,7 @@
 
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }; #pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrains Mono Nerd Font";
       };
       sansSerif = {
