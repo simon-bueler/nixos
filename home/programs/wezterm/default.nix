db6ff822e17c037561500e1d791fe2981fbe28a4
simon-bueler <simon.bueeler@icloud.com>
2025-05-07 19:00:21 +0200

    tweaks for wezterm
    Signed-off-by: simon-bueler <simon.bueeler@icloud.com>

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -1,5 +1,14 @@
 {
   programs.wezterm = {
     enable = true;
+    extraConfig = ''
+    return {
+        window_close_confirmation = "NeverPrompt",
+        --use_fancy_tab_bar = false,
+        hide_tab_bar_if_only_one_tab = true,
+        enable_scroll_bar = false,
+        window_decorations = "RESIZE",
+    };
+  '';
   };
 }
