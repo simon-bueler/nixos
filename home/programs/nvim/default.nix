6f9de4f0e639844f38ab2b03a3839c6ac52d70b3
simon-bueler <simon.bueeler@icloud.com>
2025-10-31 14:15:45 +0100

    use ThePrimeagen nvim config in nixos

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -1,10 +1,58 @@
-# Nixvim is a NixOS module that installs and configures Neovim
-{inputs, ...}: {
-  imports = [
-    inputs.nvf.homeManagerModules.default
-  ];
-
-  programs.nvf = {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  nvimWritableDir = "/home/simon/.local/share/nvim-writable";
+in {
+  programs.neovim = {
     enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
   };
+
+  home.file.".config/nvim" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "simon-bueler";
+      repo = "init.lua";
+      rev = "master"; # oder einen spezifischen commit hash
+      sha256 = "U+8S9ZDbBIR/7tFVXrZ+vm5JwsVn+wJGOIJf4TNcMjA="; # Zuerst mit falschem hash, dann korrigieren
+    };
+    recursive = true;
+  };
+
+  # Make lazy-lock.json and other generated files writable
+  home.activation.makeLazyLockWritable = config.lib.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p ${nvimWritableDir}
+
+    # Replace lazy-lock.json with symlink to writable location
+    $DRY_RUN_CMD rm -f $VERBOSE_ARG ~/.config/nvim/lazy-lock.json
+    $DRY_RUN_CMD ln -sf $VERBOSE_ARG ${nvimWritableDir}/lazy-lock.json ~/.config/nvim/lazy-lock.json
+  '';
+
+  home.packages = with pkgs; [
+    # Essential tools for the config
+    git
+    wget
+    curl
+    fd
+    ripgrep
+    fzf
+    lazygit
+    gcc
+    tree-sitter
+    nodejs
+    python3
+    unzip
+    luarocks
+    lua5_1
+    go
+
+    # LSP servers
+    rust-analyzer
+    nil
+    lua-language-server
+    gopls
+  ];
 }
